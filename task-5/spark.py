f2d7e0bebbcfbcb68540c6419bfbe4bd6b3483f7
vlerdman <erdman.vovaa@gmail.com>
2023-12-19 22:26:58 +0300

    fix spark.py

--- task-5/spark.py
+++ task-5/spark.py
@@ -5,7 +5,7 @@ import pyspark.sql.types as tp
 
 spark = SparkSession.builder.master("local").appName("hdfs_test").getOrCreate()
 
-booksSchema = StructType().add("id", "integer").add("price", tp.LongType()).add("room_id", "integer")                        .add("street_id", "integer")
+booksSchema = StructType().add("id", "integer").add("price", tp.LongType()).add("room_id", "integer").add("street_id", "integer")
 booksdata=spark.read.csv("hdfs://main:9000/mydata/sd.csv", schema=booksSchema)
 booksdata.show(5)
 booksdata.printSchema()
