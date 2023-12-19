5e6a68cb97f972f7e8aa72ecdb56e0cf97dc2ffb
vlerdman <erdman.vovaa@gmail.com>
2023-12-19 22:22:14 +0300

    add spark script fot task-5

---
+++ task-5/spark.py
@@ -0,0 +1,12 @@
+import pyspark 
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
+import pyspark.sql.types as tp
+
+spark = SparkSession.builder.master("local").appName("hdfs_test").getOrCreate()
+
+booksSchema = StructType().add("id", "integer").add("price", tp.LongType()).add("room_id", "integer")                        .add("street_id", "integer")
+booksdata=spark.read.csv("hdfs://main:9000/mydata/sd.csv", schema=booksSchema)
+booksdata.show(5)
+booksdata.printSchema()
+booksdata.write.json("hdfs://main:9000/mydata/sd.json")
